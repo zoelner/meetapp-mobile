1f564398437fe08c65afdb39ffc7eb246a8a1643
Everton Zoelner <everton.zoelner@gmail.com>
2019-08-18 01:35:53 -0300

    Adicionado guia de estilos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { StatusBar } from 'react-native';
 import App from './App';
 
 export default function Root() {
   return (
-    <Fragment>
+    <>
       <StatusBar barStyle="light-content" backgroundColor="#22202C" />
       <App />
-    </Fragment>
+    </>
   );
 }
