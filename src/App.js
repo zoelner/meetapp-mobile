5382472af811e6d73622aa3168a3d47960b8317b
Everton Zoelner <everton.zoelner@gmail.com>
2019-08-21 23:55:23 -0300

    Importado Storage do Meetapp Web

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import '~/config/ReactotronConfig';
 
 import createRouter from './routes';
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
+
+import '~/config/ReactotronConfig';
+
+import { store, persistor } from './store';
 import App from './App';
 
 export default function Root() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#22202C" />
-      <App />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#22202C" />
+        <App />
+      </PersistGate>
+    </Provider>
   );
 }
